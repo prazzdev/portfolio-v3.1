ca8d2d9186d2380382eb77840fdd0458cd64f464
prazzdev <prazzid31@gmail.com>
2023-09-07 17:58:40 +0700

    modified:   src/components/Layout/index.jsx

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -17,15 +17,15 @@ export default function Layout(props) {
             <meta name="title" content="Agung Prasetyo | Junior Front End Web Developer" />
             <meta name="description" content="As a junior front end web developer, I'm passionate about crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I'm eager to contribute to creating visually appealing and responsive websites. I'm dedicated to staying up-to-date with the latest web development trends and technologies, always striving to enhance my skills and deliver high-quality web solutions. Let's collaborate and turn your web ideas into reality together!" />
             <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://metatags.io/" />
+            <meta property="og:url" content="https://agungpraz31.my.id/" />
             <meta property="og:title" content="Agung Prasetyo | Junior Front End Web Developer" />
             <meta property="og:description" content="As a junior front end web developer, I'm passionate about crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I'm eager to contribute to creating visually appealing and responsive websites. I'm dedicated to staying up-to-date with the latest web development trends and technologies, always striving to enhance my skills and deliver high-quality web solutions. Let's collaborate and turn your web ideas into reality together!" />
-            <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+            <meta property="og:image" content="https://agungpraz31.my.id/images/meta-tags.png" />
             <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://metatags.io/" />
+            <meta property="twitter:url" content="https://agungpraz31.my.id/" />
             <meta property="twitter:title" content="Agung Prasetyo | Junior Front End Web Developer" />
             <meta property="twitter:description" content="As a junior front end web developer, I'm passionate about crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I'm eager to contribute to creating visually appealing and responsive websites. I'm dedicated to staying up-to-date with the latest web development trends and technologies, always striving to enhance my skills and deliver high-quality web solutions. Let's collaborate and turn your web ideas into reality together!" />
-            <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+            <meta property="twitter:image" content="https://agungpraz31.my.id/images/me2.svg" />
             <title>{pageTitle}</title>
         </Head>
         <Header />
