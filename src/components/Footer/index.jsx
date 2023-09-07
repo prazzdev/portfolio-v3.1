dd2c56c5a495aa4c088a9706135d2f7cd47cfdea
prazzdev <prazzid31@gmail.com>
2023-09-07 10:14:45 +0700

    modified:   src/components/Footer/index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     const getYear = getTheDate.getFullYear()
     return (
         <>
-        <footer className="fixed bottom-0 right-0 left-0 text-center bg-white py-3">
+        <footer className="fixed bottom-0 right-0 left-0 text-xs lg:text-[16px] text-center bg-white py-3">
             copyboth © {getYear} - Made with <span className="text-red-500">❤</span> by prazzdev
         </footer>
         </>
