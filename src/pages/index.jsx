72b22619788023fd1f7840a38707cb9b78042a93
prazzdev <prazzid31@gmail.com>
2023-09-07 10:11:31 +0700

    modified:   src/components/Layout/index.jsx 	modified:   src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <Layout pageTitle="Agung Prasetyo | Junior Fullstack Web Developer">
-      <div className="flex flex-col-reverse lg:flex-row justify-center items-center mx-auto w-[100%] lg:h-100vh">
+      <div className="flex flex-col-reverse lg:flex-row justify-center items-center mx-auto w-[100%]  lg:h-100vh">
         <div className="sm:flex flex-col lg:mr-[150px] mt-10 px-10 lg:p-0">
           <h1 className="font-bold text-[25px] lg:text-[35px] text-center lg:text-left">
             Agung Prasetyo
@@ -16,7 +16,7 @@ export default function Home() {
             Hi,everyone. Welcome to my portfolio website.
           </p>
         </div>
-        <div className="h-[280px] w-[280px] lg:-mt-[50px]">
+        <div className="h-[280px] w-[280px] lg:-mt-[50px] bg-teal-200 lg:bg-none rounded-xl lg:rounded-none">
           <Image 
             src={"/images/me/me1.svg"} 
             width={500}
