555e8b2bab0a37ebab2ba59d785ea81f7edca9ad
prazzdev <prazzid31@gmail.com>
2023-09-07 18:03:00 +0700

    modified:   src/components/Layout/index.jsx

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -20,12 +20,13 @@ export default function Layout(props) {
             <meta property="og:url" content="https://agungpraz31.my.id/" />
             <meta property="og:title" content="Agung Prasetyo | Junior Front End Web Developer" />
             <meta property="og:description" content="As a junior front end web developer, I'm passionate about crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I'm eager to contribute to creating visually appealing and responsive websites. I'm dedicated to staying up-to-date with the latest web development trends and technologies, always striving to enhance my skills and deliver high-quality web solutions. Let's collaborate and turn your web ideas into reality together!" />
-            <meta property="og:image" content="https://agungpraz31.my.id/images/meta-tags.png" />
+            <meta property="og:image" content="https://agungpraz31.my.id/images/me2.png" />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://agungpraz31.my.id/" />
             <meta property="twitter:title" content="Agung Prasetyo | Junior Front End Web Developer" />
             <meta property="twitter:description" content="As a junior front end web developer, I'm passionate about crafting engaging and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I'm eager to contribute to creating visually appealing and responsive websites. I'm dedicated to staying up-to-date with the latest web development trends and technologies, always striving to enhance my skills and deliver high-quality web solutions. Let's collaborate and turn your web ideas into reality together!" />
             <meta property="twitter:image" content="https://agungpraz31.my.id/images/me2.png" />
+            <link rel="icon" type="image/x-icon" href="https://agungpraz31.my.id/images/me2.png" />
             <title>{pageTitle}</title>
         </Head>
         <Header />
