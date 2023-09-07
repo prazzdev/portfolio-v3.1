5dae27fa98086255fb949c7f914853283cf49403
prazzdev <prazzid31@gmail.com>
2023-09-07 10:52:41 +0700

    modified:   src/components/NavMenu/index.jsx

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -17,11 +17,11 @@ export default function NavMenu() {
                 {/* <li><a>Sidebar Item 1</a></li>
                 <li><a>Sidebar Item 2</a></li> */}
                 <div id="headerMenu" className="flex flex-col w-[120px]">
-                    <Link href="/" className="font-bold px-5 py-2 bg-white rounded-md shadow-md mb-3 transition hover:bg-gray-100">Home</Link>
-                    <Link href="/about" className="font-bold px-4 py-2 bg-white rounded-md shadow-md mb-3 transition hover:bg-gray-100">About</Link>
-                    <Link href="/skills" className="font-bold px-4 py-2 bg-white rounded-md shadow-md mb-3 transition hover:bg-gray-100">Skills</Link>
-                    <Link href="/projects" className="font-bold px-4 py-2 bg-white rounded-md shadow-md mb-3 transition hover:bg-gray-100">Projects</Link>
-                    <Link href="https://agungpraz.hashnode.dev" className="font-bold px-4 py-2 bg-white rounded-md shadow-md mb-3 transition hover:bg-gray-100" target="_blank">Blog</Link>
+                    <Link href="/" className="font-bold px-5 py-2 bg-white text-gray-800 rounded-md shadow-md mb-3 transition hover:bg-gray-100">Home</Link>
+                    <Link href="/about" className="font-bold px-4 py-2 bg-white text-black rounded-md shadow-md mb-3 transition hover:bg-gray-100">About</Link>
+                    <Link href="/skills" className="font-bold px-4 py-2 bg-white text-black rounded-md shadow-md mb-3 transition hover:bg-gray-100">Skills</Link>
+                    <Link href="/projects" className="font-bold px-4 py-2 bg-white text-black rounded-md shadow-md mb-3 transition hover:bg-gray-100">Projects</Link>
+                    <Link href="https://agungpraz.hashnode.dev" className="font-bold px-4 py-2 bg-white text-black rounded-md shadow-md mb-3 transition hover:bg-gray-100" target="_blank">Blog</Link>
                 </div>
                 
                 </ul>
